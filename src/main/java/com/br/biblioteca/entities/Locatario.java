1f74e4e2035f7621bb6eb8c27e66a115a4d790a0
brunna.dornelles <brunnadornelles336@gmail.com>
2025-11-06 18:15:06 -0300

    Commit final do projeto, com banco de dados MySQL devidamente configurado e README.MD atualizado.

--- src/main/java/com/br/biblioteca/entities/Locatario.java
+++ src/main/java/com/br/biblioteca/entities/Locatario.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "nome", uniqueConstraints = {
+@Table(name = "Locatario", uniqueConstraints = {
         @UniqueConstraint(columnNames = "telefone")
 })
 @Data
