84a4607d2e3e2f7aeb04a918e2c4fd77c4922151
brunna.dornelles <brunnadornelles336@gmail.com>
2025-11-11 14:19:44 -0300

    Correção.

--- src/main/java/com/br/biblioteca/repositories/LocatarioRepository.java
+++ src/main/java/com/br/biblioteca/repositories/LocatarioRepository.java
@@ -1,8 +1,9 @@
 package com.br.biblioteca.repositories;
 
+import com.br.biblioteca.model.Locatario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocatarioRepository extends JpaRepository <LocatarioRepository,Long> {
+public interface LocatarioRepository extends JpaRepository <Locatario,Long> {
 }
