683a3e2fc75cbf6477d442b05aaa2e54e6aa56f2
brunna.dornelles <brunnadornelles336@gmail.com>
2025-11-11 14:05:44 -0300

    Renomeando classe corretamente.

--- src/main/java/com/br/biblioteca/repositories/LocatarioRepository.java
+++ src/main/java/com/br/biblioteca/repositories/LocatarioRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Locatario extends JpaRepository <Locatario,Long> {
+public interface LocatarioRepository extends JpaRepository <LocatarioRepository,Long> {
 }
