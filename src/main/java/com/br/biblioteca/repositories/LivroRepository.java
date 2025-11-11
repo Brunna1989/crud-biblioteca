75bc662926ceca2e77f90448e2e02a3fac800586
brunna.dornelles <brunnadornelles336@gmail.com>
2025-11-11 10:03:07 -0300

    Reiniciando projeto - De acordo com méritras de feedback técnico.

--- src/main/java/com/br/biblioteca/repositories/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.biblioteca.repositories;
-
-import com.br.biblioteca.entities.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivroRepository extends JpaRepository<Livro,Long> {
-}
--- src/main/java/com/br/biblioteca/repositories/LocatarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.biblioteca.repositories;
-
-import com.br.biblioteca.entities.Locatario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocatarioRepository extends JpaRepository<Locatario,Long> {
-}
